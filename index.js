4e47d439ee5e711d634eda6cb627b27b4ea30c8d
sean <seanbruce520@gmail.com>
2020-04-27 08:55:48 +0800

    merge changes in topic1

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function sayHello(name) {
     console.log(`Hello, I'm ${name}. How are you`);
     console.log('nice to meet your');
+    console.log('change in topic1 once);
+    console.log('change in topic2 once);
+    console.log('change in topic3 once);
 }
 
