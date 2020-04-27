12aa04727d950f237999bf90c401999c98fc750e
sean <seanbruce520@gmail.com>
2020-04-27 09:02:18 +0800

    change in topic2

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ function sayHello(name) {
     console.log(`Hello, I'm ${name}. How are you`);
     console.log('nice to meet your');
     console.log('change in topic1 once);
-    console.log('change in topic2 once);
-    console.log('change in topic3 once);
+    console.log('change in topic1 twice);
+    console.log('change in topic1 third);
+    console.log('change in topic2 once
 }
 
